41236ad329cf1e7b52f13ec9b551b80e9b700c77
johoon-Choi <johoon4687@gmail.com>
2023-03-19 13:55:53 +0900

    23.03.18 (토)

--- Algorithm/2042 구간 합 구하기.cpp
+++ Algorithm/2042 구간 합 구하기.cpp
@@ -9,34 +9,56 @@ ll c;
 ll arr[1 << 20];
 ll seg[1 << 21];
 
-ll init(int node, int s, int e)
-{
+// Full Binary Tree 형태의 Binary Tree 생성
+ll init(int node, int s, int e) {
     if (s == e) return seg[node] = arr[s];
     int mid = (s + e) / 2;
     return seg[node] = init(node << 1, s, mid) + init((node << 1) + 1, mid + 1, e);
 }
 
-ll prefixSum(int node, int s, int e, int l, int r) // 시작노드, start, end, left, right
-{
-    if (e < l || r < s) return 0; // 찾아야하는 구간(l~r)과 현재 구간(s~e)이 포함되지 않을때
-    if (l <= s && e <= r) return seg[node]; // 찾아야하는 구간(l~r)내에 현재 구간(s~e)이 포함될 때
+//                     curS   curE   tarL          tarR
+ll prefixSum(int node, int s, int e, const int& L, const int& R) {
+    if (e < L || R < s) return 0;
+    /*      target  :               (L) ================ (R)
+     *      cur1    :                                       (s) ===== (e)
+     *      cur2    : (s) ===== (e)
+     */
+
+
+    if (L <= s && e <= R) return seg[node];
+    /*      target  : (L) ================ (R)
+     *      cur     :        (s) ===== (e)
+     */
+
+     /*      [Cur status]
+      * 
+      *      target  :       (L) ================ (R)
+      *      cur1    : (s) ========= (e)
+      *   or cur2    :                  (s) ======= (e)
+      * 
+      *   그래서, 다시 반 쪼개서 체크
+      */
     int mid = (s + e) / 2;
-    // 찾아야하는 구간(l~r)이 현재 구간(s~e)에 포함되거나, 부분적으로 겹치는 경우
-    return prefixSum(2 * node, s, mid, l, r) + prefixSum(2 * node + 1, mid + 1, e, l, r);
+    
+    return   prefixSum(node << 1, s, mid, L, R) 
+           + prefixSum((node << 1) + 1, mid + 1, e, L, R);
 }
 
-void changeNum(int node, int s, int e, int index, ll diff) {// 시작노드, start, end, 찾아야하는 곳(index), 변화량(diff)
-    if (e < index || index < s) return; // 찾아야하는 곳(index)과 현재 구간(s~e)이 포함되지 않을때
+void changeNum(int node, int s, int e, const int& index, ll diff) {
+    if (e < index || index < s) return;
+
+    // change 하려는 index 가 포함된 범위의 node 는 전부 diff 를 더해준다.
+
     seg[node] += diff;
+
     if (s != e) {
         int mid = (s + e) / 2;
-        changeNum(2 * node, s, mid, index, diff);
-        changeNum(2 * node + 1, mid + 1, e, index, diff);
+        changeNum(  node << 1,        s,          mid,    index,  diff);
+        changeNum(  (node << 1) + 1,  mid + 1,    e,      index,  diff);
     }
 }
 
-int main()
-{
+int main() {
     cin >> N >> M >> K;
 
     for (int i = 1; i <= N; i++)
@@ -44,13 +66,14 @@ int main()
 
     init(1, 1, N); // 세그먼트 트리 생성
 
-    for (int i = 0; i < M + K; i++)
-    {
+    for (int i = 0; i < M + K; i++) {
         cin >> a >> b >> c;
+
         ll diff = 0;
+
         switch (a) {
         case 1:
-            diff = c - arr[b];
+            diff = c - arr[b];  // arr[b] + diff == c
             arr[b] = c;
             changeNum(1, 1, N, b, diff);
             break;
